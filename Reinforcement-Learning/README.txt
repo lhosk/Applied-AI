2669c65793b420e348e20af00a896e21c8e17f48
Lucas H <lhoskin@charlotte.edu>
2025-04-05 15:25:01 -0400

    New RL

---
+++ Reinforcement-Learning/README.txt
@@ -0,0 +1,16 @@
+## Reinforcement Learning using Pong and Freeway
+
+### Drive Folder:
+- https://drive.google.com/drive/folders/1XjubvDh8hbRX1wKfxGC6966yua3KurDC?usp=drive_link
+
+### Pong Gameplay:
+- https://drive.google.com/file/d/18woYmc9AByH8ZEgzZEyutfeABx8_JaEW/view?usp=drive_link
+
+### Freeway Gameplay:
+- https://drive.google.com/file/d/1dTw0WvQ-8DJlmfuc6nU2yqoBTZh3HMg3/view?usp=drive_link
+
+### Pong Model (Trained):
+- https://drive.google.com/file/d/17XLFz-F5rUA-Nf7EzqhUU-IW6xePLW-R/view?usp=drive_link
+
+### Freeway Model (Trained):
+- https://drive.google.com/file/d/1qtPa8PD55L5ZkplQjUQ9XfSmfH64VJ-t/view?usp=drive_link
---
+++ Reinforcement-Learning/freeway_model_801.py
@@ -0,0 +1,32 @@
+# UNC Charlotte
+# ITCS 5153 - Applied AI - Spring 2025
+# Lab 5
+# Reinforcement Learning
+# This module implements the game Freeway
+# Student ID: 801
+
+
+from stable_baselines3.common.env_util import make_atari_env
+from stable_baselines3.common.vec_env import VecFrameStack
+from stable_baselines3 import A2C
+
+import ale_py
+
+# There already exists an environment generator
+# that will make and wrap atari environments correctly.
+# Here we are also multi-worker training (n_envs=12 => 12 environments)
+vec_env = make_atari_env("ALE/Freeway-v5", n_envs=12, seed=0)
+# Frame-stacking with 4 frames
+vec_env = VecFrameStack(vec_env, n_stack=4)
+
+model = A2C("CnnPolicy", vec_env, verbose=1)
+model.learn(total_timesteps=100_000)
+
+# Save the model
+model.save("freeway_model_801")
+
+obs = vec_env.reset()
+while True:
+    action, _states = model.predict(obs, deterministic=False)
+    obs, rewards, dones, info = vec_env.step(action)
+    vec_env.render("human")
